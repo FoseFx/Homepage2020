a20e849ad0848047b3b0b06ea4bafc6ba6291553
Max Baumann <info@fosefx.com>
2021-04-16 10:50:13 +0200

    add anti FLoC header

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { 'http-equiv': 'Permissions-Policy', content: 'interest-cohort=()' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
