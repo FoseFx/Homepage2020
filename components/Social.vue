c755cd865d33da903aab4faf3d09f8619af0200b
Max Baumann <max@bmn.dev>
2021-12-09 21:40:27 +0100

    fix: fix pgp link

--- components/Social.vue
+++ components/Social.vue
@@ -179,7 +179,7 @@
         </g>
       </svg>
     </a>
-    <a href="/pgp"
+    <a href="/max@bmn.dev.asc"
       target="__blank"
       style="margin-left: 0.5rem">
       <svg aria-label="PGP Key" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M12.65 10C11.83 7.67 9.61 6 7 6c-3.31 0-6 2.69-6 6s2.69 6 6 6c2.61 0 4.83-1.67 5.65-4H17v4h4v-4h2v-4H12.65zM7 14c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>
