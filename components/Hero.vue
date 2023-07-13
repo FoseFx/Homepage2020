b0751139322a3ef7b39302b653396703c05caa49
Max Baumann <max@bmn.dev>
2023-07-13 17:35:25 +0200

    update tagline

--- components/Hero.vue
+++ components/Hero.vue
@@ -7,7 +7,7 @@
           <span id="hey">Hey!</span><br />
           <span id="max" class="fresh-anim">I'm Max</span>
         </h1>
-        <h2>Web Developer</h2>
+        <h2>Software Engineer</h2>
         <Button @click="goToAbout()">Read more</Button>
       </div>
     </div>
